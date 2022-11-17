084e30751441f695d5261028634e28b57258f932
Daniel Huang <danielhuang3214@gmail.com>
2022-11-17 13:50:33 -0500

    First Commit

---
+++ src/firebase/index.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA9ONZA6blR_6Ne-TDT5rEiK1HTDra6lg4",
+  authDomain: "ics4u-643b3.firebaseapp.com",
+  projectId: "ics4u-643b3",
+  storageBucket: "ics4u-643b3.appspot.com",
+  messagingSenderId: "554718882970",
+  appId: "1:554718882970:web:3cdde4ff0470b09a854323"
+};
+
+const app = initializeApp(firebaseConfig);
