ecfa8d405b36303dace2fb5ee56cbee70d683661
Daniel Huang <danielhuang3214@gmail.com>
2023-01-15 14:13:27 -0500

    Added a way to login

---
+++ src/views/LoginView.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { ref } from "vue";
+
+const email = ref("");
+const password = ref("");
+
+const auth = getAuth();
+console.log(auth);
+const login = async () => {
+  await signInWithEmailAndPassword(auth, email.value, password.value)
+    .then((userCredential) => {
+      console.log(userCredential);
+      console.log("yes");
+    })
+    .catch((error) => {
+      console.log(error.code);
+      console.log(error.message);
+    })
+};
+</script>
+
+<template>
+  <div>
+    <h1>Login</h1>
+    <form @submit.prevent="login()">
+      <input v-model="email" type="email" placeholder="email" /> <br />
+      <input v-model="password" type="password" placeholder="password" />
+      <input type="submit" value="Login" />
+    </form>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
